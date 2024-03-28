ca91344e291de98ce932163f4b5822d2312eee9f
LENOVO <LENOVO@WH-VKPERU>
2024-03-27 22:17:44 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declaramos nuestras variables
+int option=0;
+Double result=0;
+
+
+// Dibujamos el menú para que el usuario pueda ver que opciones tiene.
+Console.WriteLine("1 - Calcular el area del cuadrado");
+Console.WriteLine("2 - Calcular el volumen del cubo");
+Console.WriteLine("3 - Salir");
+// Guardamos la variable
+option = Convert.ToInt32(Console.ReadLine());
+
+
+// Evaluamos la opción elegida y retornamos los resultados.
+switch (option)
+{  
+       
+    case 4:        
+    case 5:   
+    case 6:
+        result = 1;
+        break;
+    case 7:        
+    case 8:        
+    case 9:
+        result = 2;
+        break;
+    case 10:       
+    case 11:        
+    case 12:
+        result = 3;
+        break;
+}
+
+// Imprimimos el mensaje.
+Console.WriteLine(result);
+
+
+
